85766bdf37952a818a8e441e07ce50c8782219d0
donald.s <Dj4101961>
2019-09-25 12:32:24 -0400

    postgres db migration setup

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/rishi.k/rishi_code/bugbashapp/venv/bin/python3.7
+#!/Users/donald.s/workspace/bugbashapp/venv/bin/python3.6
 from django.core import management
 
 if __name__ == "__main__":
