f5839708ff6ed5ad5a8d170095c02823b0208bd5
donald.s <Dj4101961>
2019-12-11 15:36:46 -0500

    clean up comments

--- users/views.py
+++ users/views.py
@@ -22,24 +22,6 @@ def login_view(request):
 			login(request, user)
 			return HttpResponseRedirect('/home/')
 
-
-
-		# try:
-		#     user = fdb.authe.sign_in_with_email_and_password(email, password)
-		#     message = "WELCOME TO THE BUG BASH {}".format(fdb.database.child("users").child(user[
-		#                                                                                     'localId']).child("details").child("First").get().val().capitalize())
-		#     #user.save()
-		#     return HttpResponseRedirect('/home/', messages.error(request, message))
-		# except Exception as e:
-		#     error_json = e.args[1]
-		#     error = json.loads(error_json)['error']
-		#     print(error['message'])
-		#     if "INVALID_PASSWORD" in error['message']:
-		#         message = "Wrong password. Please try again."
-		#         return redirect('/login/', messages.error(request, message))
-		#     else:
-		#         message = "Email address not registered. Please register."
-		#         return redirect('/register/', messages.error(request, message))
 	else:
 		return render(request, 'login.html', {'user_login': user_login})
 
@@ -50,28 +32,14 @@ def register(request):
 	if request.method == 'POST':
 
 		if user_form.is_valid():
-			# user_form.save()
+
 			custom_user = CustomUserManager()
 			user = custom_user.create_user(email=user_form.data.get('email'), password=user_form.data.get('password'))
 			user.save()
 			print(user.email)
 
 			return HttpResponseRedirect('/login/', "Success: You are registered!")
-				# user = fdb.authe.create_user_with_email_and_password(user_form.data.get('email'),
-				# 												 user_form.data.get(
-				# 													 'password'))
-				# uid = user['localId']
-				# message = "Success: You are registered!"
-				# # Add data from user_form to a dictionary
-				# data = {"First": user_form.data.get('first_name'),
-				# 		"Last": user_form.data.get('last_name'), "Email": user_form.data.get(
-				# 		'email'),
-				# 		"Role": user_form.data.get('role')}
-				# #user_form.save()
-				#
-				# # Add dictionary details of user to the database
-				# fdb.database.child("users").child(uid).child("details").set(data, user['idToken'])
-				# return HttpResponseRedirect('/login/', messages.error(request, message))
+
 
 	else:
 		user_form = UserForm()
