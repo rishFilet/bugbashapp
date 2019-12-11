ce0e952884bb4e3df384a42e56a74fbe9dd421e7
Narasimhan Pranatharthi <narap@ecobee.com>
2019-12-11 16:54:22 -0500

    Add unit tests for resolvers

---
+++ bugbashapp/tests.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.urls import resolve
+
+from report.views import create_report
+from users.views import login_view, register
+
+
+class ResolversTest(TestCase):
+
+    def test_login_url_resolves_to_login_view(self):
+        view = resolve('/login/')
+        self.assertEquals(view.func, login_view)
+
+    def test_home_url_resolves_to_home_view(self):
+        view = resolve('/home/')
+        self.assertEquals(view.func, create_report)
+
+    def test_register_url_resolves_to_register_view(self):
+        view = resolve('/register/')
+        self.assertEquals(view.func, register)
+
