9d0a25ac59d69460e90e973b1e52ecd8f723e8ae
Rishi Khan <rishi.k@ecobee.com>
2019-12-13 14:18:29 -0500

    Added favicon and script for running test before commit

---
+++ commit_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python $PWD/manage.py test
+echo Continue with commit? [y/n]
+read answer 
+if [ "$answer" == "y" ]
+then
+    echo What is your commit message?
+    read m
+    git commit -m "$m"
+    git push
+    echo "Damn you are lit..thanks for pushing"
+elif [ "$answer" == "n" ]
+then
+    echo "Commitment can be hard sometimes. Goodbye"
+else
+    echo Invalid Answer, try again
+fi
