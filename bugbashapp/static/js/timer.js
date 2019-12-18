d6dc732310d80d6be8946d3f60f027c121ddcf9d
Rishi Khan <rishi.k@ecobee.com>
2019-12-18 11:44:01 -0500

    Last minute changes

--- bugbashapp/static/js/timer.js
+++ bugbashapp/static/js/timer.js
@@ -1,4 +1,4 @@
-$("#countdown").countdown({until: +0, format: 'MS', onExpiry: liftOff});
+$("#countdown").countdown({until: +400, format: 'MS', onExpiry: liftOff});
 		function liftOff(){
 			$(".popup-overlay, .popup-content").addClass("active");
 			$(".submit-btn").css("visibility", "hidden");
