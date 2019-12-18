1ca4c1328250223e9151b26273f7b107b69644c7
Narasimhan Pranatharthi <narap@ecobee.com>
2019-12-18 11:08:38 -0500

    Fix bug report js

--- bugbashapp/static/js/main.js
+++ bugbashapp/static/js/main.js
@@ -30,7 +30,18 @@ $(function() {
                 $('#id_result').val(''); // remove the value from the                                                     input
                 console.log("success"); // another sanity check
                 if (response.summary) {
-                    $("#userBugTableBody").append('<tr id="bug-'+response.id+'"><td>'+response.summary+'</td><td align="center"><button  class="btn btn-primary" onclick="editBug('+response.id+')" data-toggle="modal" data-target="#myModal">EDIT</button></td><td align="center"><button class="btn btn-danger form-control" onClick="deleteBug({{bug.id}})">DELETE</button></td>');
+                    var row =
+                    $("#userBugTableBody").append(`
+                        <tr id="bug-${response.id}">
+                            <td class="bugSummary">${response.summary}</td>
+                            '<td align="center">
+                                <button  class="btn btn-primary" onclick="editBug(${response.id})" data-toggle="modal" data-target="#myModal">EDIT</button>
+                             </td>
+                             <td align="center">
+                                <button class="btn btn-danger form-control" onClick="deleteBug(${response.id})">DELETE</button>
+                             </td>
+                         </tr>
+                         `);
                 };
             },
 //            // handle a non-successful response
